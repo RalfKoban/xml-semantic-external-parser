0841bfb1839eefc453f3cae6466456f3e9b07472
Ralf Koban <rkn@mm-software.com>
2018-07-13 09:18:15 +0000

    Traverser created

---
+++ Parser/Traverser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+using MiKoSolutions.SemanticParsers.Xml.Yaml;
+
+namespace MiKoSolutions.SemanticParsers.Xml
+{
+    public static class Traverser
+    {
+        public static IEnumerable<ContainerOrTerminalNode> Traverse(File file)
+        {
+            var results = new List<ContainerOrTerminalNode>();
+
+            Fill(file.Children, results);
+
+            return results;
+        }
+
+        private static void Fill(IEnumerable<ContainerOrTerminalNode> nodes, List<ContainerOrTerminalNode> results)
+        {
+            foreach (var node in nodes)
+            {
+                Fill(node, results);
+            }
+        }
+
+        private static void Fill(ContainerOrTerminalNode node, List<ContainerOrTerminalNode> results)
+        {
+            results.Add(node);
+
+            if (node is Container c)
+            {
+                Fill(c.Children, results);
+            }
+        }
+    }
+}
