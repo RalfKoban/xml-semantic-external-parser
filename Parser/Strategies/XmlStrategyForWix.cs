986aa94c641cdedd3d367c9bb330d608ecacf3aa
Ralf Koban <rkn@mm-software.com>
2018-07-30 14:52:41 +0000

    refactoring, more WIX elements added for choice to be a terminal node

--- Parser/Strategies/XmlStrategyForWix.cs
+++ Parser/Strategies/XmlStrategyForWix.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml;
+﻿using System.Collections.Generic;
+using System.Xml;
 
 using MiKoSolutions.SemanticParsers.Xml.Yaml;
 
@@ -6,50 +7,89 @@ namespace MiKoSolutions.SemanticParsers.Xml.Strategies
 {
     public sealed class XmlStrategyForWix : XmlStrategy
     {
+        private static readonly HashSet<string> TerminalNodeNames = new HashSet<string>
+                                                                        {
+                                                                            "ApprovedExeForElevation",
+                                                                            "AllocateRegistrySpace",
+                                                                            "AppSearch",
+                                                                            "AssemblyName",
+                                                                            "BinaryRef",
+                                                                            "BindImage",
+                                                                            "Catalog",
+                                                                            "CCPSearch",
+                                                                            "Column",
+                                                                            "CommandLine",
+                                                                            "ComponentGroupRef",
+                                                                            "ComponentRef",
+                                                                            "Condition",
+                                                                            "Configuration",
+                                                                            "ConfigurationData",
+                                                                            "ContainerRef",
+                                                                            "CopyFile",
+                                                                            "CostFinalize",
+                                                                            "CostInitialize",
+                                                                            "CreateFolders",
+                                                                            "CreateShortcuts",
+                                                                            "Custom",
+                                                                            "CustomAction",
+                                                                            "CustomActionRef",
+                                                                            "CustomProperty",
+                                                                            "Data",
+                                                                            "DeleteServices",
+                                                                            "Dependency",
+                                                                            "DialogRef",
+                                                                            "DigitalCertificate",
+                                                                            "DigitalCertificateRef",
+                                                                            "DisableRollback",
+                                                                            "DuplicateFiles",
+                                                                            "EmbeddedChainer",
+                                                                            "EmbeddedChainerRef",
+                                                                            "EmbeddedUIResource",
+                                                                            "EnsureTable",
+                                                                            "Environment",
+                                                                            "Error",
+                                                                            "Exclusion",
+                                                                            "ExecuteAction",
+                                                                            "ExitCode",
+                                                                            "Failure",
+                                                                            "FeatureGroupRef",
+                                                                            "FileCost",
+                                                                            "FileSearch",
+                                                                            "FileSearchRef",
+                                                                            "FileTypeMask",
+                                                                            "FindRelatedProducts",
+                                                                            "ForceReboot",
+                                                                            "Icon",
+                                                                            "IconRef",
+                                                                            "IgnoreModularization",
+                                                                            "IgnoreRange",
+                                                                            "IgnoreTable",
+                                                                            "IniFile",
+                                                                            "InstallAdminPackage",
+                                                                            "InstallExecute",
+                                                                            "InstallExecuteAgain",
+                                                                            "InstallFiles",
+                                                                            "InstallFinalize",
+                                                                            "InstallInitialize",
+                                                                            "InstallODBC",
+                                                                            "InstallServices",
+                                                                            "InstallValidate",
+                                                                            "Instance",
+                                                                            "Interface",
+                                                                            "IsolateComponent",
+                                                                            "IsolateComponents",
+                                                                            "MajorUpgrade",
+                                                                            "MergeRef",
+                                                                            "UIRef",
+                                                                            "Variable",
+                                                                        };
+
         public override bool ParseAttributesEnabled => false;
 
         public override string GetName(XmlTextReader reader) => reader.NodeType == XmlNodeType.Element ? reader.GetAttribute("Id") ?? reader.Name : base.GetName(reader);
 
         public override string GetType(XmlTextReader reader) => reader.NodeType == XmlNodeType.Element ? reader.Name : base.GetType(reader);
 
-        public override bool ShallBeTerminalNode(Container container)
-        {
-            switch (container?.Type)
-            {
-                case "ApprovedExeForElevation":
-                case "AllocateRegistrySpace":
-                case "AppSearch":
-                case "AssemblyName":
-                case "BinaryRef":
-                case "BindImage":
-                case "Catalog":
-                case "CCPSearch":
-                case "Column":
-                case "CommandLine":
-                case "ComponentGroupRef":
-                case "ComponentRef":
-                case "Condition":
-                case "Configuration":
-                case "ConfigurationData":
-                case "ContainerRef":
-                case "CopyFile":
-                case "CostFinalize":
-                case "CostInitialize":
-                case "CreateFolders":
-                case "CreateShortcuts":
-                case "Custom":
-                case "DuplicateFiles":
-                case "Icon":
-                case "IconRef":
-                case "MajorUpgrade":
-                case "MergeRef":
-                case "UIRef":
-                case "Variable":
-                    return true;
-
-                default:
-                    return false;
-            }
-        }
+        public override bool ShallBeTerminalNode(Container container) => TerminalNodeNames.Contains(container?.Type);
     }
 }
