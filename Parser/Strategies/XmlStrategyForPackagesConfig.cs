ae99fa8f45d14115b3d5e2bab53d95fc43df2e0c
Ralf Koban <rkn@mm-software.com>
2018-07-31 07:52:41 +0000

    dots added for packages.config texts

--- Parser/Strategies/XmlStrategyForPackagesConfig.cs
+++ Parser/Strategies/XmlStrategyForPackagesConfig.cs
@@ -17,7 +17,7 @@ namespace MiKoSolutions.SemanticParsers.Xml.Strategies
                         return reader.Name;
                     }
 
-                    return $"{reader.Name} \"{TrimLength(value, 20)}\"";
+                    return $"{reader.Name} '{TrimLength(value, 20)}'";
                 }
 
                 case XmlNodeType.Attribute:
@@ -25,7 +25,7 @@ namespace MiKoSolutions.SemanticParsers.Xml.Strategies
                     var readerName = reader.Name;
                     var value = reader.Value;
 
-                    return $"{readerName}=\"{TrimLength(value, 20)}\"";
+                    return $"{readerName} '{TrimLength(value, 20)}'";
                 }
 
                 default:
@@ -37,6 +37,14 @@ namespace MiKoSolutions.SemanticParsers.Xml.Strategies
 
         public override string GetType(XmlTextReader reader) => reader.NodeType == XmlNodeType.Element ? reader.Name : base.GetType(reader);
 
-        private static string TrimLength(string value, int maxLength) => value.Substring(0, Math.Min(value.Length, maxLength));
+        private static string TrimLength(string value, int maxLength, string suffix = "...")
+        {
+            if (value.Length > maxLength)
+            {
+                return value.Substring(0, maxLength) + suffix;
+            }
+
+            return value;
+        }
     }
 }
