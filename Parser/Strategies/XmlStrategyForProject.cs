cbfdb835c8c699b25da9c49a3345df8ce622c3c7
Ralf Koban <rkn@mm-software.com>
2018-08-02 12:52:54 +0000

    terminal nodes for .csproj files added

--- Parser/Strategies/XmlStrategyForProject.cs
+++ Parser/Strategies/XmlStrategyForProject.cs
@@ -1,11 +1,100 @@
-﻿using System.Xml;
+﻿using System.Collections.Generic;
+using System.Xml;
+
+using MiKoSolutions.SemanticParsers.Xml.Yaml;
 
 namespace MiKoSolutions.SemanticParsers.Xml.Strategies
 {
     public sealed class XmlStrategyForProject : XmlStrategy
     {
-        public override string GetName(XmlTextReader reader) => reader.NodeType == XmlNodeType.Element ? reader.Name : base.GetName(reader);
+        private static readonly HashSet<string> TerminalNodeNames = new HashSet<string>
+                                                                        {
+                                                                            "Analyzer",
+                                                                            "AppDesignerFolder",
+                                                                            "AssemblyName",
+                                                                            "AssemblyOriginatorKeyFile",
+                                                                            "BootstrapperPackage",
+                                                                            "CodeAnalysisDictionary",
+                                                                            "CodeAnalysisRuleSet",
+                                                                            "CodeAnalysisTreatWarningsAsErrors",
+                                                                            "Compile",
+                                                                            "Configuration",
+                                                                            "Content",
+                                                                            "DebugSymbols",
+                                                                            "DebugType",
+                                                                            "DefineConstants",
+                                                                            "DocumentationFile",
+                                                                            "EmbeddedResource",
+                                                                            "Error",
+                                                                            "ErrorReport",
+                                                                            "ErrorText",
+                                                                            "FileAlignment",
+                                                                            "FileUpgradeFlags",
+                                                                            "Import",
+                                                                            "LangVersion",
+                                                                            "None",
+                                                                            "Optimize",
+                                                                            "OutputPath",
+                                                                            "OutputType",
+                                                                            "Platform",
+                                                                            "PostSharpTargetProcessor",
+                                                                            "Prefer32Bit",
+                                                                            "PostBuildEvent",
+                                                                            "PreBuildEvent",
+                                                                            "ProjectGuid",
+                                                                            "ProjectReference",
+                                                                            "Reference",
+                                                                            "RestorePackages",
+                                                                            "RootNamespace",
+                                                                            "RunCodeAnalysis",
+                                                                            "SignAssembly",
+                                                                            "SolutionDir",
+                                                                            "StyleCopEnabled",
+                                                                            "StyleCopTreatErrorsAsWarnings",
+                                                                            "TargetFrameworkProfile",
+                                                                            "TargetFrameworkVersion",
+                                                                            "TreatWarningsAsErrors",
+                                                                            "WarningLevel",
+                                                                            "OldToolsVersion",
+                                                                            "UpgradeBackupLocation",
+                                                                            "PublishUrl",
+                                                                            "Install",
+                                                                            "InstallFrom",
+                                                                            "UpdateEnabled",
+                                                                            "UpdateMode",
+                                                                            "UpdateInterval",
+                                                                            "UpdateIntervalUnits",
+                                                                            "UpdatePeriodically",
+                                                                            "UpdateRequired",
+                                                                            "MapFileExtensions",
+                                                                            "ApplicationRevision",
+                                                                            "ApplicationVersion",
+                                                                            "IsWebBootstrapper",
+                                                                            "UseApplicationTrust",
+                                                                            "BootstrapperEnabled",
+                                                                            "ProductVersion",
+                                                                            "SchemaVersion",
+                                                                            "Warning",
+                                                                        };
+
+        public override bool ParseAttributesEnabled => false;
+
+        public override string GetName(XmlTextReader reader)
+        {
+            if (reader.NodeType == XmlNodeType.Element)
+            {
+                var name = reader.Name;
+
+                // var identifier = reader.GetAttribute("Include");
+                // return identifier is null ? name : $"{name} '{identifier}'";
+                return name;
+            }
+
+            return base.GetName(reader);
+        }
 
         public override string GetType(XmlTextReader reader) => reader.NodeType == XmlNodeType.Element ? reader.Name : base.GetType(reader);
+
+        public override bool ShallBeTerminalNode(Container container) => TerminalNodeNames.Contains(container?.Type);
     }
 }
