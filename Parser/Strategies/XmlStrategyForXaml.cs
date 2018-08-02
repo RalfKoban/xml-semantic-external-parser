dd82549947b97de29ad59d7dfc355968965806f3
Ralf Koban <rkn@mm-software.com>
2018-08-02 13:33:53 +0000

    trial for XAMLs to use specific elements as terminal nodes (even if they contain others)

--- Parser/Strategies/XmlStrategyForXaml.cs
+++ Parser/Strategies/XmlStrategyForXaml.cs
@@ -14,6 +14,7 @@ namespace MiKoSolutions.SemanticParsers.Xml.Strategies
                                                                             "ColumnDefinition",
                                                                             "CheckBox",
                                                                             "Image",
+                                                                            "KeyBinding",
                                                                             "Label",
                                                                             "RowDefinition",
                                                                             "Setter",
