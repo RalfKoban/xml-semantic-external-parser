4fb3ca559ca68c916a143019f86988b9d13fe502
Ralf Koban <rkn@mm-software.com>
2018-08-09 15:46:28 +0000

    XML flavor for FxCop CustomDictionary added

--- Parser/Flavors/XmlFlavorFinder.cs
+++ Parser/Flavors/XmlFlavorFinder.cs
@@ -7,6 +7,7 @@ namespace MiKoSolutions.SemanticParsers.Xml.Flavors
     {
         private static readonly XmlFlavor[] Flavors =
                                                         {
+                                                            new XmlFlavorForFxCop(),
                                                             new XmlFlavorForPackagesConfig(),
                                                             new XmlFlavorForProject(),
                                                             new XmlFlavorForNDepend(),
---
+++ Parser/Flavors/XmlFlavorForFxCop.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+using MiKoSolutions.SemanticParsers.Xml.Yaml;
+
+namespace MiKoSolutions.SemanticParsers.Xml.Flavors
+{
+    public class XmlFlavorForFxCop : XmlFlavor
+    {
+        private static readonly HashSet<string> TerminalNodeNames = new HashSet<string>
+                                                                           {
+                                                                               "Acronym",
+                                                                               "Term",
+                                                                               "Word",
+                                                                           };
+
+        public override bool ParseAttributesEnabled => false;
+
+        public override bool Supports(string filePath) => filePath.StartsWith("CustomDictionary", StringComparison.OrdinalIgnoreCase) && filePath.EndsWith(".xml", StringComparison.OrdinalIgnoreCase);
+
+        public override bool Supports(DocumentInfo info) => string.Equals(info.RootElement, "Dictionary", StringComparison.OrdinalIgnoreCase);
+
+        public override string GetType(XmlTextReader reader) => reader.NodeType == XmlNodeType.Element ? reader.Name : base.GetType(reader);
+
+        public override ContainerOrTerminalNode FinalAdjustAfterParsingComplete(ContainerOrTerminalNode node)
+        {
+            if (node is Container c)
+            {
+                var textNode = c.Children.FirstOrDefault(_ => _.Type == NodeType.Text);
+                if (textNode != null)
+                {
+                    c.Name = textNode.Content;
+                }
+            }
+
+            return base.FinalAdjustAfterParsingComplete(node);
+        }
+
+        protected override bool ShallBeTerminalNode(ContainerOrTerminalNode node) => TerminalNodeNames.Contains(node?.Type);
+    }
+}
