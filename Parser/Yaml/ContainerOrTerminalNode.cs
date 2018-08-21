a613470f1cf2ba4dc91998e6cd2e6d702c9f91ad
Ralf Koban <rkn@mm-software.com>
2018-08-21 10:01:54 +0000

    WorkaroundForRegexIssue moved

--- Parser/Yaml/ContainerOrTerminalNode.cs
+++ Parser/Yaml/ContainerOrTerminalNode.cs
@@ -7,11 +7,25 @@ namespace MiKoSolutions.SemanticParsers.Xml.Yaml
     [DebuggerDisplay("Type={Type}, Name={Name}")]
     public abstract class ContainerOrTerminalNode
     {
+        [YamlIgnore]
+        private string _type;
+
+        [YamlIgnore]
+        private string _name;
+
         [YamlMember(Alias = "type", Order = 1)]
-        public string Type { get; set; }
+        public string Type
+        {
+            get => _type;
+            set => _type = WorkaroundForRegexIssue(value);
+        }
 
         [YamlMember(Alias = "name", Order = 2)]
-        public string Name { get; set; }
+        public string Name
+        {
+            get => _name;
+            set => _name = WorkaroundForRegexIssue(value);
+        }
 
         [YamlMember(Alias = "locationSpan", Order = 3)]
         public LocationSpan LocationSpan { get; set; }
@@ -22,5 +36,7 @@ namespace MiKoSolutions.SemanticParsers.Xml.Yaml
         public abstract CharacterSpan GetTotalSpan();
 
         public abstract TerminalNode ToTerminalNode();
+
+        private static string WorkaroundForRegexIssue(string value) => value?.Replace("\\", " \\ "); // workaround for Semantic/GMaster RegEx parsing exception that is not aware of special backslash character sequences
     }
 }
