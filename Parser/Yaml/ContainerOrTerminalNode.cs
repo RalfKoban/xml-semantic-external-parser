1392237fd7acb8eb4d02c06e4752114fd4033586
Ralf Koban <rkn@mm-software.com>
2018-08-24 13:54:31 +0000

    Performance optimizations (strings now get inlined)

--- Parser/Yaml/ContainerOrTerminalNode.cs
+++ Parser/Yaml/ContainerOrTerminalNode.cs
@@ -13,30 +13,37 @@ namespace MiKoSolutions.SemanticParsers.Xml.Yaml
         [YamlIgnore]
         private string _name;
 
+        [YamlIgnore]
+        private string _content;
+
         [YamlMember(Alias = "type", Order = 1)]
         public string Type
         {
             get => _type;
-            set => _type = WorkaroundForRegexIssue(value);
+            set => _type = value is null ? null : string.Intern(WorkaroundForRegexIssue(value)); // performance optimization for large files
         }
 
         [YamlMember(Alias = "name", Order = 2)]
         public string Name
         {
             get => _name;
-            set => _name = WorkaroundForRegexIssue(value);
+            set => _name = value is null ? null : string.Intern(WorkaroundForRegexIssue(value)); // performance optimization for large files
         }
 
         [YamlMember(Alias = "locationSpan", Order = 3)]
         public LocationSpan LocationSpan { get; set; }
 
         [YamlIgnore]
-        public string Content { get; set; }
+        public string Content
+        {
+            get => _content;
+            set => _content = value is null ? null : string.IsInterned(value) ?? value; // performance optimization for large files (if we have the string interned, we can re-use it)
+        }
 
         public abstract CharacterSpan GetTotalSpan();
 
         public abstract TerminalNode ToTerminalNode();
 
-        private static string WorkaroundForRegexIssue(string value) => value?.Replace("\\", " \\ "); // workaround for Semantic/GMaster RegEx parsing exception that is not aware of special backslash character sequences
+        private static string WorkaroundForRegexIssue(string value) => value.Replace("\\", " \\ "); // workaround for Semantic/GMaster RegEx parsing exception that is not aware of special backslash character sequences
     }
 }
