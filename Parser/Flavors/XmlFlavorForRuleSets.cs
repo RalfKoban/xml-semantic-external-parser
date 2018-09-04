733b77834d96309ffef1bdc1f7adc049508c77f1
Ralf Koban <rkn@mm-software.com>
2018-09-04 10:39:57 +0000

    Support for rulesets enhanced

---
+++ Parser/Flavors/XmlFlavorForRuleSets.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+using MiKoSolutions.SemanticParsers.Xml.Yaml;
+
+namespace MiKoSolutions.SemanticParsers.Xml.Flavors
+{
+    public sealed class XmlFlavorForRuleSets : XmlFlavor
+    {
+        private const string IncludeAll = "IncludeAll";
+        private const string RuleSet = "RuleSet";
+        private const string Rules = "Rules";
+        private const string Rule = "Rule";
+
+        private static readonly HashSet<string> TerminalNodeNames = new HashSet<string>
+                                                                        {
+                                                                            IncludeAll,
+                                                                            Rule,
+                                                                        };
+
+        public override bool ParseAttributesEnabled => false;
+
+        public override bool Supports(string filePath) => filePath.EndsWith(".ruleset", StringComparison.OrdinalIgnoreCase);
+
+        public override bool Supports(DocumentInfo info) => string.Equals(info.RootElement, RuleSet, StringComparison.OrdinalIgnoreCase);
+
+        public override string GetName(XmlTextReader reader)
+        {
+            if (reader.NodeType == XmlNodeType.Element)
+            {
+                var name = reader.Name;
+                var attributeName = GetAttribute(name);
+                var identifier = string.IsNullOrWhiteSpace(attributeName) ? null : reader.GetAttribute(attributeName);
+                return identifier ?? name;
+            }
+
+            return base.GetName(reader);
+        }
+
+        public override string GetType(XmlTextReader reader) => reader.NodeType == XmlNodeType.Element ? reader.Name : base.GetType(reader);
+
+        protected override bool ShallBeTerminalNode(ContainerOrTerminalNode node) => TerminalNodeNames.Contains(node?.Type);
+
+        private static string GetAttribute(string name)
+        {
+            switch (name)
+            {
+                case RuleSet: return "Name";
+                case Rules: return "AnalyzerId";
+                case Rule: return "Id";
+                default: return null;
+            }
+        }
+    }
+}
---
+++ Tests/ParserTests_RuleSet.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using MiKoSolutions.SemanticParsers.Xml.Yaml;
+
+using NUnit.Framework;
+
+namespace MiKoSolutions.SemanticParsers.Xml
+{
+    [TestFixture]
+    public class ParserTests_RuleSet
+    {
+        private Yaml.File _objectUnderTest;
+        private Yaml.Container _root;
+
+        [SetUp]
+        public void PrepareTest()
+        {
+            var parentDirectory = Directory.GetParent(new Uri(GetType().Assembly.Location).LocalPath).FullName;
+            var fileName = Path.Combine(parentDirectory, "Resources", "RuleSet.xml");
+
+            _objectUnderTest = Parser.Parse(fileName);
+            _root = _objectUnderTest.Children.Single();
+        }
+
+        [Test]
+        public void Root_has_correct_name() => Assert.That(_root.Name, Is.EqualTo("Microsoft Managed Recommended Rules"));
+
+        [TestCase("Rules", "Microsoft.Analyzers.ManagedCodeAnalysis")]
+        [TestCase("Rules", "StyleCop.Analyzers")]
+        public void Rules_group_is_found_properly(string groupType, string name)
+        {
+            var item = _root.Children.OfType<Container>().Where(_ => _.Type == groupType).Any(_ => _.Name == name);
+
+            Assert.That(item, Is.True);
+        }
+
+        [TestCase("Rules", "Microsoft.Analyzers.ManagedCodeAnalysis", "CA1001")]
+        [TestCase("Rules", "Microsoft.Analyzers.ManagedCodeAnalysis", "CA1009")]
+        [TestCase("Rules", "StyleCop.Analyzers", "SA1120")]
+        public void Rule_is_found_properly(string groupType, string name, string ruleName)
+        {
+            var item = _root.Children.OfType<Container>().Where(_ => _.Type == groupType && _.Name == name).SelectMany(_ => _.Children).Any(_ => _.Name == ruleName);
+
+            Assert.That(item, Is.True);
+        }
+
+        [Test]
+        public void RoundTrip_does_not_report_parsing_errors()
+        {
+            var builder = new StringBuilder();
+            using (var writer = new StringWriter(builder))
+            {
+                YamlWriter.Write(writer, _objectUnderTest);
+            }
+
+            Assert.That(builder.ToString(), Does.Contain("parsingErrorsDetected: false"));
+        }
+    }
+}
