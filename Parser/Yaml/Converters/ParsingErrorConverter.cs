8f7b8bac79b97697481ae40412ad685954f5db08
Ralf Koban <rkn@mm-software.com>
2018-09-05 18:05:51 +0000

    Pattern matching used

--- Parser/Yaml/Converters/ParsingErrorConverter.cs
+++ Parser/Yaml/Converters/ParsingErrorConverter.cs
@@ -14,27 +14,28 @@ namespace MiKoSolutions.SemanticParsers.Xml.Yaml.Converters
 
         public void WriteYaml(IEmitter emitter, object value, Type type)
         {
-            var error = value as ParsingError;
-            if (error == null)
+            if (value is ParsingError error)
+            {
+                emitter.Emit(new MappingStart(null, null, true, MappingStyle.Block));
+
+                // location
+                emitter.Emit(new Scalar("location"));
+
+                emitter.Emit(new SequenceStart(null, null, false, SequenceStyle.Flow));
+                emitter.Emit(new Scalar(error.Location.LineNumber.ToString()));
+                emitter.Emit(new Scalar(error.Location.LinePosition.ToString()));
+                emitter.Emit(new SequenceEnd());
+
+                // message
+                emitter.Emit(new Scalar("message"));
+                emitter.Emit(new Scalar(null, null, error.ErrorMessage, ScalarStyle.SingleQuoted, false, true));
+
+                emitter.Emit(new MappingEnd());
+            }
+            else
             {
                 throw new NotImplementedException("wrong type");
             }
-
-            emitter.Emit(new MappingStart(null, null, true, MappingStyle.Block));
-
-            // location
-            emitter.Emit(new Scalar("location"));
-
-            emitter.Emit(new SequenceStart(null, null, false, SequenceStyle.Flow));
-            emitter.Emit(new Scalar(error.Location.LineNumber.ToString()));
-            emitter.Emit(new Scalar(error.Location.LinePosition.ToString()));
-            emitter.Emit(new SequenceEnd());
-
-            // message
-            emitter.Emit(new Scalar("message"));
-            emitter.Emit(new Scalar(null, null, error.ErrorMessage, ScalarStyle.SingleQuoted, false, true));
-
-            emitter.Emit(new MappingEnd());
         }
     }
 }
