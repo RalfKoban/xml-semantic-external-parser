28c9dbc9b50e5dce0976aecc29aa22bfd54f8389
Ralf Koban <rkn@mm-software.com>
2019-03-26 17:49:36 +0000

    Test file added

---
+++ Tests/ParserTests_NLS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace MiKoSolutions.SemanticParsers.Xml
+{
+    [TestFixture]
+    public sealed class ParserTests_NLS
+    {
+        private Yaml.File _objectUnderTest;
+        private Yaml.Container _root;
+
+        [SetUp]
+        public void PrepareTest()
+        {
+            var parentDirectory = Directory.GetParent(new Uri(GetType().Assembly.Location).LocalPath).FullName;
+            var fileName = Path.Combine(parentDirectory, "Resources", "NLS1.xml");
+
+            _objectUnderTest = Parser.Parse(fileName);
+            _root = _objectUnderTest.Children.Single();
+        }
+
+        [Test]
+        public void File_Name_matches()
+        {
+            Assert.That(_objectUnderTest.Name, Does.EndWith(Path.DirectorySeparatorChar + "NLS1.xml"));
+        }
+    }
+}
