1e950190895c8dd576c94a817b99ff296ff07a84
Ralf Koban <rkn@mm-software.com>
2019-04-16 14:31:37 +0000

    Add support for more elements

--- Parser/Flavors/XmlFlavorForPOM.cs
+++ Parser/Flavors/XmlFlavorForPOM.cs
@@ -6,6 +6,7 @@ using MiKoSolutions.SemanticParsers.Xml.Yaml;
 
 namespace MiKoSolutions.SemanticParsers.Xml.Flavors
 {
+    /// <a href="https://maven.apache.org/xsd/maven-4.0.0.xsd" />
     public sealed class XmlFlavorForPOM : XmlFlavor
     {
         private static readonly HashSet<string> TerminalNodeNames = new HashSet<string>
@@ -14,11 +15,29 @@ namespace MiKoSolutions.SemanticParsers.Xml.Flavors
                                                                             ElementNames.GroupId,
                                                                             ElementNames.ArtifactId,
                                                                             ElementNames.Version,
+                                                                            ElementNames.Parent,
                                                                             ElementNames.Packaging,
                                                                             ElementNames.Name,
                                                                             ElementNames.Description,
                                                                             ElementNames.Url,
                                                                             ElementNames.InceptionYear,
+                                                                            ElementNames.Organization,
+                                                                            ElementNames.License,
+                                                                            ElementNames.Developer,
+                                                                            ElementNames.Contributor,
+                                                                            ElementNames.MailingList,
+                                                                            ElementNames.Prerequisites,
+                                                                            ElementNames.Module,
+                                                                            ElementNames.Scm,
+                                                                            ElementNames.IssueManagement,
+                                                                            ElementNames.CiManagement,
+                                                                            ElementNames.DistributionManagement,
+                                                                            ElementNames.DependencyManagement,
+                                                                            ElementNames.Dependency,
+                                                                            ElementNames.Repository,
+                                                                            ElementNames.PluginRepository,
+                                                                            ElementNames.Reporting,
+                                                                            ElementNames.Profile,
                                                                         };
 
         public override bool ParseAttributesEnabled => false;
@@ -51,6 +70,7 @@ namespace MiKoSolutions.SemanticParsers.Xml.Flavors
         {
             internal const string Project = "project";
             internal const string ModelVersion = "modelVersion";
+            internal const string Parent = "parent";
             internal const string GroupId = "groupId";
             internal const string ArtifactId = "artifactId";
             internal const string Version = "version";
@@ -59,6 +79,23 @@ namespace MiKoSolutions.SemanticParsers.Xml.Flavors
             internal const string Description = "description";
             internal const string Url = "url";
             internal const string InceptionYear = "inceptionYear";
+            internal const string Organization = "organization";
+            internal const string License = "license";
+            internal const string Developer = "developer";
+            internal const string Contributor = "contributor";
+            internal const string MailingList = "mailingList";
+            internal const string Prerequisites = "prerequisites";
+            internal const string Module = "module";
+            internal const string Scm = "scm";
+            internal const string IssueManagement = "issueManagement";
+            internal const string CiManagement = "ciManagement";
+            internal const string DistributionManagement = "distributionManagement";
+            internal const string DependencyManagement = "dependencyManagement";
+            internal const string Dependency = "dependency";
+            internal const string Repository = "repository";
+            internal const string PluginRepository = "pluginRepository";
+            internal const string Reporting = "reporting";
+            internal const string Profile = "profile";
         }
 
         private static class AttributeNames
