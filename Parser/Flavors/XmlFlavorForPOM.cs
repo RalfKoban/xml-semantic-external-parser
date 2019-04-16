c89892f026a37982578305cbb4f134e94058a451
Ralf Koban <rkn@mm-software.com>
2019-04-16 15:21:08 +0000

    Add support for more elements

--- Parser/Flavors/XmlFlavorForPOM.cs
+++ Parser/Flavors/XmlFlavorForPOM.cs
@@ -29,16 +29,22 @@ namespace MiKoSolutions.SemanticParsers.Xml.Flavors
                                                                             ElementNames.MailingList,
                                                                             ElementNames.Prerequisites,
                                                                             ElementNames.Module,
-                                                                            ElementNames.Scm,
-                                                                            ElementNames.IssueManagement,
                                                                             ElementNames.CiManagement,
                                                                             ElementNames.DistributionManagement,
                                                                             ElementNames.DependencyManagement,
                                                                             ElementNames.Dependency,
                                                                             ElementNames.Repository,
                                                                             ElementNames.PluginRepository,
+                                                                            ElementNames.Plugin,
                                                                             ElementNames.Reporting,
-                                                                            ElementNames.Profile,
+                                                                            ElementNames.Connection,
+                                                                            ElementNames.DeveloperConnection,
+                                                                            ElementNames.ArtifactsToPublish,
+                                                                            ElementNames.ArtifactsToDownload,
+                                                                            ElementNames.Tag,
+                                                                            ElementNames.GitRepositoryName,
+                                                                            ElementNames.System,
+                                                                            ElementNames.Id,
                                                                         };
 
         public override bool ParseAttributesEnabled => false;
@@ -48,18 +54,7 @@ namespace MiKoSolutions.SemanticParsers.Xml.Flavors
         public override bool Supports(DocumentInfo info) => string.Equals(info.RootElement, ElementNames.Project, StringComparison.OrdinalIgnoreCase)
                                                          && string.Equals(info.Namespace, "http://maven.apache.org/POM/4.0.0", StringComparison.OrdinalIgnoreCase);
 
-        public override string GetName(XmlTextReader reader)
-        {
-            if (reader.NodeType == XmlNodeType.Element)
-            {
-                var name = reader.LocalName;
-                var attr = GetAttributeName(name);
-
-                return attr is null ? name : reader.GetAttribute(attr);
-            }
-
-            return base.GetName(reader);
-        }
+        public override string GetName(XmlTextReader reader) => reader.NodeType == XmlNodeType.Element ? reader.LocalName : base.GetName(reader);
 
         public override string GetType(XmlTextReader reader) => reader.NodeType == XmlNodeType.Element ? reader.Name : base.GetType(reader);
 
@@ -67,19 +62,26 @@ namespace MiKoSolutions.SemanticParsers.Xml.Flavors
         {
             if (node is Container c)
             {
-                switch (node.Type)
+                var nodeType = node.Type;
+                switch (nodeType)
                 {
+                    case ElementNames.ArtifactId:
                     case ElementNames.GroupId:
                     case ElementNames.Module:
                     case ElementNames.Name:
+                    case ElementNames.Id:
+                    case ElementNames.Url:
+                    case ElementNames.Connection:
+                    case ElementNames.DeveloperConnection:
+                    case ElementNames.Tag:
+                    case ElementNames.GitRepositoryName:
+                    case ElementNames.System:
                         node.Name = c.Children.FirstOrDefault(_ => _.Type == NodeType.Text)?.Content;
                         break;
-                    case ElementNames.Developer:
-                    case ElementNames.License:
-                        node.Name = c.Children.FirstOrDefault(_ => _.Type == ElementNames.Name)?.Name;
-                        break;
+
                     default:
-                        node.Name = c.Children.FirstOrDefault(_ => _.Type == ElementNames.GroupId)?.Name;
+                        var type = GetNamingElementType(nodeType);
+                        node.Name = c.Children.FirstOrDefault(_ => _.Type == type)?.Name;
                         break;
                 }
             }
@@ -89,7 +91,24 @@ namespace MiKoSolutions.SemanticParsers.Xml.Flavors
 
         protected override bool ShallBeTerminalNode(ContainerOrTerminalNode node) => TerminalNodeNames.Contains(node?.Type);
 
-        private string GetAttributeName(string elementName) => null;
+        private static string GetNamingElementType(string nodeType)
+        {
+            switch (nodeType)
+            {
+                case ElementNames.Developer:
+                case ElementNames.License:
+                case ElementNames.Organization:
+                    return ElementNames.Name;
+
+                case ElementNames.Profile:
+                    return ElementNames.Id;
+
+                case ElementNames.IssueManagement:
+                    return ElementNames.System;
+                default:
+                    return ElementNames.GroupId;
+            }
+        }
 
         private static class ElementNames
         {
@@ -119,12 +138,17 @@ namespace MiKoSolutions.SemanticParsers.Xml.Flavors
             internal const string Dependency = "dependency";
             internal const string Repository = "repository";
             internal const string PluginRepository = "pluginRepository";
+            internal const string Plugin = "plugin";
             internal const string Reporting = "reporting";
             internal const string Profile = "profile";
-        }
-
-        private static class AttributeNames
-        {
+            internal const string Id = "id";
+            internal const string Connection = "connection";
+            internal const string DeveloperConnection = "developerConnection";
+            internal const string ArtifactsToPublish = "artifactsToPublish";
+            internal const string ArtifactsToDownload = "artifactsToDownload";
+            internal const string Tag = "tag";
+            internal const string GitRepositoryName = "gitRepositoryName";
+            internal const string System = "system";
         }
     }
 }
