85b03580e0419b78e768b5dacee6d58ec70e7c04
Ralf Koban <rkn@mm-software.com>
2020-03-24 12:05:34 +0000

    Added support for .rxrep (Ranorex repository CXMLs)

---
+++ Parser/Flavors/XmlFlavorForRanorexRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Xml;
+using MiKoSolutions.SemanticParsers.Xml.Yaml;
+
+namespace MiKoSolutions.SemanticParsers.Xml.Flavors
+{
+    public sealed class XmlFlavorForRanorexRepository : XmlFlavor
+    {
+        public override bool ParseAttributesEnabled => false;
+
+        public override bool Supports(string filePath) => filePath.EndsWith(".rxrep", StringComparison.OrdinalIgnoreCase);
+
+        public override bool Supports(DocumentInfo info) => string.Equals(info.RootElement, "repository", StringComparison.OrdinalIgnoreCase);
+
+        public override string GetName(XmlReader reader)
+        {
+            if (reader.NodeType == XmlNodeType.Element)
+            {
+                return reader.GetAttribute("name") ?? reader.LocalName;
+            }
+
+            return base.GetName(reader);
+        }
+
+        public override string GetType(XmlReader reader) => reader.NodeType == XmlNodeType.Element ? reader.LocalName : base.GetType(reader);
+
+        protected override bool ShallBeTerminalNode(ContainerOrTerminalNode node)
+        {
+            switch (node?.Type)
+            {
+                case "item":
+                case "icon":
+                case "basepath":
+                case "var":
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+    }
+}
